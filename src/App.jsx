4b9f55b250ac9b6c84d13175aa7a3e6c67d20a76
Web-Martin <martin052017@outlook.com>
2024-03-11 08:55:29 -0400

    Functionality were added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,59 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [balance, setBalance] = useState(0);
+  const [inputAmount, setInputAmount] = useState("");
+  const [userAction, setUserAction] = useState("expense");
+
+  // Set values
+  const getValueInput = (e) => {
+    let value = parseInt(e.target.value);
+    setInputAmount(value);
+  };
+  const getSelectValue = (e) => {
+    setUserAction(e.target.value);
+  };
+
+  // Operations
+  const restBalance = () => {
+    setBalance(balance - inputAmount);
+  };
+
+  const addBalance = () => {
+    setBalance(balance + inputAmount);
+  };
+
+  //Functionality
+  const action = () => {
+    switch (userAction) {
+      case "income":
+        addBalance();
+        setInputAmount("");
+
+        break;
+      case "expense":
+        restBalance();
+        setInputAmount("");
+
+        break;
+      default:
+        console.log("Sorry you have to select one");
+    }
+  };
+
   return (
     <>
       <div id="user-money">
-        <select name="activity" id="activity">
-          <option value="food">Food</option>
+        <select name="activity" id="activity" onChange={getSelectValue}>
+          <option value="expense">Expense</option>
           <option value="income">Income</option>
         </select>
-        <input type="number" />
-        <button>Add</button>
+        <input type="number" value={inputAmount} onChange={getValueInput} />
+        <button onClick={action}>Add</button>
       </div>
       <div id="current-balance">
-        <p>$0.00</p>
+        <p>{`$${balance}`}</p>
         <span>Current Balance</span>
       </div>
     </>
